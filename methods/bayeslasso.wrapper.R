60b0cc66516db168b1f1b15f798a39ee4ece06fd
xiangzhu <xiangzhu.nku@gmail.com>
2015-03-02 22:40:27 -0600

    update the wrapper of BLR package

--- methods/bayeslasso.wrapper.R
+++ methods/bayeslasso.wrapper.R
@@ -4,40 +4,66 @@
 
 bayeslasso.wrapper = function(input, args){
 
-  # load in the dataset
-  y = input$y
-  X = input$X
-  A = input$Z
+  # locate the dataset
+  data.name = input$data.name
+  data.path = paste0('/home/maimaizhu/GitHub/dscr_blm/datamakers/', data.name, '/')
 
-  # defaul setting
-  nIter = 5500
-  burnIn = 500
-  thin = 1
+  # load in the dataset
+  genotype.path = paste0(data.path, 'genotype.RData')
+  load(genotype.path)
+
+  phenotype.path = paste0(data.path, 'phenotype_', input$phenotypeID, '.RData')
+  load(phenotype.path)
+
+  if (length(intersect(list.files(), 'relatedness.RData'))==0){
+	z = NULL
+  }else{
+	relatedness.path = paste0(data.path, 'relatedness.RData')
+	load(relatedness.path)
+  }
+
+  # specify the genotype matrix, phenotype vector and relatedness matrix
+  observedID = names(y)
+  Y = y
+  X = x[rownames(x) %in% observedID, ]
+  A = z[rownames(x) %in% observedID, rownames(x) %in% observedID]
+  if (prod(names(Y) == names(X)) == 0)
+	stop("individuals are not the same in genotype and phenotype")
+
+  # defaul setting for blr routine
+  #nIter = 5500
+  #burnIn = 500
+  #thin = 1
   priorBL = list(varE=list(df=3,S=2.5),
 		 varU=list(df=3,S=0.63),
 		 lambda = list(shape=0.52,rate=1e-5,value=20,type='random'))
 
-  # user-specified setting
+  # user-specified setting for blr routine
   if(!is.null(args$nIter))
 	nIter = args$nIter
   if(!is.null(args$burnIn))
 	burnIn = args$burnIn
   if(!is.null(args$thin))
 	thin = args$thin
-  if(length(priorBL) != 0)
+  if(length(args$priorBL) != 0)
 	priorBL = args$priorBL
 
   # create a test set: rmv the observed phenotype
-  whichNa = input$row.test
-  yNa = y
-  yNa[whichNa] = NA
+  whichNA = input$test.subject
+  YNA = Y
+  YNA[whichNA] = NA
 
   # run gibbs sampler to fit bayes lasso model
-  fm=BLR(y=yNa, XL=X, GF=list(ID=(1:nrow(A)),A=A), prior=priorBL, nIter=nIter, burnIn=burnIn, thin=thin)
+  library(BLR)
+  if (is.null(A)){
+  	fm=BLR(y=YNA, XL=X, GF=NULL, prior=priorBL, nIter=nIter, burnIn=burnIn, thin=thin)
+  }else{
+	fm=BLR(y=YNA, XL=X, GF=list(ID=(1:nrow(A)),A=A), prior=priorBL, nIter=nIter, burnIn=burnIn, thin=thin)
+  }
 
   # output the posterior mean as predicted value
-  y.predict = fm$yHat[whichNa]
-  y.true = y[whichNa]
+  y.predict = fm$yHat[whichNA]
+  y.true = y[whichNA]
   return(list(y.predict=y.predict, y.true=y.true)) 
 
 }
