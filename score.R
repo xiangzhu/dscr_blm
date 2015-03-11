feb67507fe8e860d7cc1dc954da8ac3dd4b5fb72
xiangzhu <xiangzhu.nku@gmail.com>
2015-03-11 12:58:45 -0500

    update score func

--- score.R
+++ score.R
@@ -2,5 +2,11 @@
 
 score = function(data, output){
 #insert calculations here; return a named list of results
-  return(list())
+  true.value = data$meta$true.value
+  predict = output$predict
+  mean_squared_error = mean( (true.value - predict)^2 )
+  mean_absolute_error = mean( abs(true.value - predict) )
+  return(list(mean_squared_error=mean_squared_error, mean_absolute_error=mean_absolute_error))
 }
+
+addScore(dsc_osl,score)
