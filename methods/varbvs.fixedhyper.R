e537b1302e9097191cf3bf6282e0c97cdd88d907
xiangzhu <xiangzhu.nku@gmail.com>
2015-03-11 11:36:05 -0500

    replace prdt func with prdt value in varbvs wrapper

--- methods/varbvs.fixedhyper.R
+++ methods/varbvs.fixedhyper.R
@@ -6,7 +6,7 @@ varbvs.fixedhyper.wrapper = function(input,args){
 
   # locate the dataset
   data.name = input$data.name
-  data.path = paste0('/home/maimaizhu/GitHub/dscr_blm/datamakers/', data.name, '/')
+  data.path = paste0('../datamakers/', data.name, '/')
 
   # load in the dataset
   genotype.path = paste0(data.path, 'genotype.RData')
@@ -40,14 +40,28 @@ varbvs.fixedhyper.wrapper = function(input,args){
   X.train = X[which.train, ]
   
   # fix the hyper parameters
-  sigma = args$sigma # the variance of the residual
-  sa = args$sa # sa*sigma is the prior variance of the regression coefficients
-  logodds = args$logodds # prior log-odds of inclusion for each variable
-
+  if(!is.null(args$sigma)){
+  	sigma = args$sigma
+  }else{
+	sigma = 4 # the variance of the residual 
+  } 
+  if(!is.null(args$sa)){
+  	sa = args$sa
+  }else{ 			
+  	sa = 1/4 # sa*sigma is the prior variance of the regression coefficients
+  }
+  if(!is.null(args$logodds)){
+  	logodds = args$logodds
+  }else{ 	
+  	logodds = log(0.01/0.99) # prior log-odds of inclusion for each variable
+  }
   # fit the model by variational inference
   library(varbvs)
   result <- varbvsoptimize(X.train, y.train, sigma, sa, logodds)
 
-  # output the phenotype prediction function
-  return(result)
+  # output the phenotype prediction for the test set
+  beta.var <- result$alpha * result$mu;
+  y.test.predict = X.test %*% beta;
+
+  return(list(predict=y.test.predict))
 }
