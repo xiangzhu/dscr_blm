873bbb62ae5cdcef00d68c74556d08cb040fbde0
xiangzhu <xiangzhu.nku@gmail.com>
2015-03-11 13:16:34 -0500

    fix typos

--- methods.R
+++ methods.R
@@ -1,6 +1,6 @@
 sourceDir("methods")
-methods=list()
 #now for each method define a list with its name, function and arguments (if no additional arguments use NULL)
-# like this: 
-#methods[[1]] = list(name="methodname",fn = function,args=NULL)
- 
+
+addMethod(dsc_osl, name="bayes_lasso", fn=bayeslasso.wrapper, args=list(nIter=NULL, burnIn=NULL, thin=NULL, priorBL=list()))
+addMethod(dsc_osl, name="varbvs_fixed", fn=varbvs.fixedhyper.wrapper, args=list(sigma=NULL, sa=NULL, logodds=NULL))
+addMethod(dsc_osl, name="gemma_bslmm", fn=gemma.bslmm.wrapper, args=list(bslmm=NULL, w=NULL, s=NULL)) 
